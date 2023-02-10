a5ca709cb243d12e77961e4c76db9b60d9409c41
Daniel Munoz <dmunoz215@gmail.com>
2023-02-09 20:52:54 -0600

    test db

--- extension/popup.js
+++ extension/popup.js
@@ -2,13 +2,17 @@ var button = document.getElementById("submitButton");
 
 button.addEventListener("click", async() =>{
 	var obj1 = document.getElementsByName("fname")[0].value;
+	var obj2 = document.getElementsByName("urlName")[0].value;
 
     const req = new XMLHttpRequest();
 	const baseUrl = "http://3.141.35.128/add/";
     const urlParams = `name=${obj1}`;
+	const urlParams2 = `name=${obj2}`;
+
     req.open("POST", baseUrl, true);
     req.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
-    req.send(urlParams);
+    req.send(urlParams)
+
     req.onreadystatechange = function() { // Call a function when the state changes.
         if (this.readyState === XMLHttpRequest.DONE && this.status === 200) {
             console.log("Got response 200!");
